4c1bbff298d06dada4995ca50161d08d56ecbd59
omarfh111 <omar.fkihhassen@esprit.tn>
2025-11-10 18:54:55 +0100

    travail demande workshop 8 9 10 11

--- confsite/ConferenceApp/views.py
+++ confsite/ConferenceApp/views.py
@@ -4,6 +4,8 @@ from django.views.generic import *
 from django.urls import reverse_lazy
 from .form import *
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from .models import Submission
 # Create your views here.
 
 
@@ -39,4 +41,36 @@ class ConferenceDelete(LoginRequiredMixin,DeleteView):
     template_name="conferences/conference_confirm_delete.html"
     fields = "__all__"
     success_url = reverse_lazy("liste_conferences")
-    
+
+class ListSubmissions(LoginRequiredMixin, ListView):
+    model = Submission
+    template_name = "submissions/list.html"
+    context_object_name = "submissions"
+
+    def get_queryset(self):
+        return Submission.objects.filter(user=self.request.user)    
+class DetailSubmission(LoginRequiredMixin, DetailView):
+    model = Submission
+    template_name = "submissions/detail.html"
+    context_object_name = "submission"
+class AddSubmission(LoginRequiredMixin, CreateView):
+    model = Submission
+    fields = ["title", "abstract", "keywords", "paper", "conference"]
+    template_name = "submissions/form.html"
+    success_url = reverse_lazy("list_submissions")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.status = "submitted"
+        return super().form_valid(form)
+class UpdateSubmission(LoginRequiredMixin, UpdateView):
+    model = Submission
+    fields = ["title", "abstract", "keywords", "paper"]
+    template_name = "submissions/form.html"
+    success_url = reverse_lazy("list_submissions")
+
+    def get_object(self, queryset=None):
+        submission = super().get_object(queryset)
+        if submission.status in ["accepted", "rejected"]:
+            raise PermissionError("Vous ne pouvez pas modifier cette soumission.")
+        return submission
